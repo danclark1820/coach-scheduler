9b9bcd8ae673ef0a5db6ae32629082ac37ea4931
Da Clark <danclark1820@gmail.com>
2023-09-13 10:36:15 -0400

    Devise views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :controllers
-  devise_for :views
-  devise_for :users
+  # devise_for :controllers
+  # devise_for :views
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :slots, only: [:index]
